fbfbdd7678b8f9ca5a51449036be4a35ed60e27c
ashok pal <pala90651@gmail.com>
2018-02-10 08:50:16 +0000

    imp gulp

---
+++ src/js/controllers/main.js
@@ -0,0 +1,21 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name adminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the adminApp
+ */
+angular.module('adminApp').controller('MainCtrl', function($scope) {
+  
+        
+        $scope.collapseVar = 0;
+        $scope.check = function(x){
+          if(x==$scope.collapseVar)
+            $scope.collapseVar = 0;
+          else
+            $scope.collapseVar = x;
+        };
+        
+    
+});
